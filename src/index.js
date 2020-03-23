d7dd16869e1f4e2a59c23cecbc5e5e11ade036b3
Beck <beck.madiev@gmail.com>
2020-03-23 18:33:25 +0300

    eslint configured and fix src

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import 'core-js';
-class Animal {
-    constructor (name, age) {
-        this.name = name;
-        this.age = age;
-    }
 
-    greet () {
-        console.log(`Hello! My name is ${this.name} and I am ${this.age}`);
-    }
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello! My name is ${this.name} and I am ${this.age}`);
+  }
 }
 
 const duck = new Animal('Bruno', 20);
